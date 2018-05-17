6dbfad5a9b968d17db8ba421430e278599fc61a3
Dario Nikolic <dario@cni.net.au>
2018-05-17 12:55:10 +1000

    Enable IE File downloads

--- ARMTemplateAssets/Scripts/srv_initial_conf.ps1
+++ ARMTemplateAssets/Scripts/srv_initial_conf.ps1
@@ -8,7 +8,7 @@ netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" n
 #Enable WMI Firewall Exception on Servers
 netsh advfirewall firewall set rule group="Windows Management Instrumentation (WMI)" new enable=Yes
 
-#Enable 
+#Set the Timezone and Locale
 TZUTIL /s "AUS Eastern Standard Time"
 Set-Culture en-AU
 Set-WinUserLanguageList en-AU -Force
@@ -17,3 +17,9 @@ Set-WinHomeLocation -GeoId 12
 Set-WinUILanguageOverride -Language en-AU
 $lang = New-WinUserLanguageList -Language en-AU
 Set-WinUserLanguageList -languagelist $lang -Force
+
+#Enable IE File downloads
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
